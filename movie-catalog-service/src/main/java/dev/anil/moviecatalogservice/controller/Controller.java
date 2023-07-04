eaeea4e1b3a8682c8a981589c10892319b314027
Anil Choudhary <masteranilchoudhary@gmail.com>
2023-07-04 19:55:40 +0530

    tried async api call

--- movie-catalog-service/src/main/java/dev/anil/moviecatalogservice/controller/Controller.java
+++ movie-catalog-service/src/main/java/dev/anil/moviecatalogservice/controller/Controller.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,6 +25,9 @@ public class Controller {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @GetMapping("/{userId}")
     public ResponseEntity<List<CatalogItem>> getMovieCatalog(@PathVariable("userId") String userId){
         List<Rating> ratingsList = Arrays.asList(
@@ -32,7 +36,8 @@ public class Controller {
         );
         List<CatalogItem> response;
         response = ratingsList.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8081/movies/"+rating.getMovieId(), Movie.class);
+//            Movie movie = restTemplate.getForObject("http://localhost:8081/movies/"+rating.getMovieId(), Movie.class);
+            Movie movie = webClientBuilder.build().get().uri("http://localhost:8081/movies/"+rating.getMovieId()).retrieve().bodyToMono(Movie.class).block();
             return new CatalogItem(movie.getName(),"description", rating.getRating());
         }).collect(Collectors.toList());
 
