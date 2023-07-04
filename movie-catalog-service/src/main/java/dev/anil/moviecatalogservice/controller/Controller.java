4f0789e38c5d19be24ca7309a2aae08580bd3d4c
Anil Choudhary <masteranilchoudhary@gmail.com>
2023-07-04 19:33:20 +0530

    used RestTemplate to call movie info api

--- movie-catalog-service/src/main/java/dev/anil/moviecatalogservice/controller/Controller.java
+++ movie-catalog-service/src/main/java/dev/anil/moviecatalogservice/controller/Controller.java
@@ -1,6 +1,9 @@
 package dev.anil.moviecatalogservice.controller;
 
 import dev.anil.moviecatalogservice.models.CatalogItem;
+import dev.anil.moviecatalogservice.models.Movie;
+import dev.anil.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -8,14 +11,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class Controller {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/{userId}")
-    public ResponseEntity<CatalogItem> getMovieCatalog(@PathVariable("userId") String userId){
-        return new ResponseEntity<CatalogItem>(new CatalogItem("Transformers","Description",4), HttpStatus.OK);
+    public ResponseEntity<List<CatalogItem>> getMovieCatalog(@PathVariable("userId") String userId){
+        List<Rating> ratingsList = Arrays.asList(
+                new Rating("1234", 3),
+                new Rating("5678", 4)
+        );
+        List<CatalogItem> response;
+        response = ratingsList.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8081/movies/"+rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(),"description", rating.getRating());
+        }).collect(Collectors.toList());
+
+        return new ResponseEntity<List<CatalogItem>>(response, HttpStatus.OK);
     }
 
 }
